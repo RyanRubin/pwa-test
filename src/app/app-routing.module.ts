24afd96ed29e666fe758cd192a37996b25711e70
raessatech5 <raessatech5@gmail.com>
2022-02-28 14:29:53 +0800

    added user-list and user-details modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./user-list/user-list.module').then(m => m.UserListModule) },
+  { path: 'user-details', loadChildren: () => import('./user-details/user-details.module').then(m => m.UserDetailsModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
